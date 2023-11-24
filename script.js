8c3a4124f9d705656de9cfba9df39d30d71fb200
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-24 12:15:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function changeColor() {
+      const blockId = document.getElementById('block_id').value;
+      const color = document.getElementById('colour_id').value;
+
+      const block = document.getElementById(blockId);
+      if (block) {
+        block.style.backgroundColor = color;
+      }
+    }
+
+    function resetColors() {
+      const gridItems = document.querySelectorAll('.grid-item');
+      gridItems.forEach(item => {
+        item.style.backgroundColor = 'transparent';
+      });
+    }
