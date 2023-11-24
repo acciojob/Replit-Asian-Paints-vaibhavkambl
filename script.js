7e97d15bfe0945d64e35c20af4c622e3e6d4aacc
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-24 12:22:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 //your JS code here. If required.
-function changeColor() {
-      const blockId = document.getElementById('block_id').value;
-      const color = document.getElementById('colour_id').value;
-
-      const block = document.getElementById(blockId);
-      if (block) {
-        block.style.backgroundColor = color;
-      }
+document.getElementById("change_button").addEventListener("click", function() {
+  var block_id = document.getElementById("block_id").value;
+  var colour_id = document.getElementById("colour_id").value;
+  var block = document.getElementById(block_id);
+  block.style.backgroundColor = colour_id;
+  var grid_items = document.getElementsByClassName("grid-item");
+  for (var i = 0; i < grid_items.length; i++) {
+    if (grid_items[i].id != block_id) {
+      grid_items[i].style.backgroundColor = "transparent";
     }
-
-    function resetColors() {
-      const gridItems = document.querySelectorAll('.grid-item');
-      gridItems.forEach(item => {
-        item.style.backgroundColor = 'transparent';
-      });
-    }
+  }
+});
+document.getElementById("reset_button").addEventListener("click", function() {
+  var grid_items = document.getElementsByClassName("grid-item");
+  for (var i = 0; i < grid_items.length; i++) {
+    grid_items[i].style.backgroundColor = "transparent";
+  }
+});
